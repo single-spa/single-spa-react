9fdbaae70fcf343e0a562667257476dceb3b6ae0
Joel Denning <joel.denning@canopytax.com>
2017-12-27 17:01:30 -0700

    Warning if rootComponent doesn't implement componentDidCatch

--- src/single-spa-react.js
+++ src/single-spa-react.js
@@ -1,71 +1,72 @@
 const defaultOpts = {
-	// required opts
-	React: null,
-	ReactDOM: null,
-	rootComponent: null,
-	domElementGetter: null,
+  // required opts
+  React: null,
+  ReactDOM: null,
+  rootComponent: null,
+  domElementGetter: null,
+  suppressComponentDidCatchWarning: false,
 }
 
 export default function singleSpaReact(userOpts) {
-	if (typeof userOpts !== 'object') {
-		throw new Error(`single-spa-react requires a configuration object`);
-	}
+  if (typeof userOpts !== 'object') {
+    throw new Error(`single-spa-react requires a configuration object`);
+  }
 
-	const opts = {
-		...defaultOpts,
-		...userOpts,
-	};
+  const opts = {
+    ...defaultOpts,
+    ...userOpts,
+  };
 
-	if (!opts.React) {
-		throw new Error(`single-spa-react must be passed opts.React`);
-	}
+  if (!opts.React) {
+    throw new Error(`single-spa-react must be passed opts.React`);
+  }
 
-	if (!opts.ReactDOM) {
-		throw new Error(`single-spa-react must be passed opts.ReactDOM`);
-	}
+  if (!opts.ReactDOM) {
+    throw new Error(`single-spa-react must be passed opts.ReactDOM`);
+  }
 
-	if (!opts.rootComponent) {
-		throw new Error(`single-spa-react must be passed opts.rootComponent`);
-	}
+  if (!opts.rootComponent) {
+    throw new Error(`single-spa-react must be passed opts.rootComponent`);
+  }
 
-	if (!opts.domElementGetter) {
-		throw new Error(`single-spa-react must be passed opts.domElementGetter function`);
-	}
+  if (!opts.domElementGetter) {
+    throw new Error(`single-spa-react must be passed opts.domElementGetter function`);
+  }
 
-	return {
-		bootstrap: bootstrap.bind(null, opts),
-		mount: mount.bind(null, opts),
-		unmount: unmount.bind(null, opts),
-	};
+  return {
+    bootstrap: bootstrap.bind(null, opts),
+    mount: mount.bind(null, opts),
+    unmount: unmount.bind(null, opts),
+  };
 }
 
 function bootstrap(opts) {
-	return Promise.resolve();
+  return Promise.resolve();
 }
 
 function mount(opts, props) {
-	return new Promise((resolve, reject) => {
-		const whenFinished = resolve;
-		const renderedComponent = opts.ReactDOM.render(opts.React.createElement(opts.rootComponent), getRootDomEl(opts), whenFinished);
-    if (!renderedComponent.componentDidCatch) {
+  return new Promise((resolve, reject) => {
+    const whenFinished = resolve;
+    const renderedComponent = opts.ReactDOM.render(opts.React.createElement(opts.rootComponent), getRootDomEl(opts), whenFinished);
+    if (!renderedComponent.componentDidCatch && !opts.suppressComponentDidCatchWarning) {
       console.warn(`the rootComponent for "${props.childAppName}" that was passed to single-spa-react should implement componentDidCatch.`);
     }
-	})
+  })
 }
 
 function unmount(opts) {
-	return Promise
-		.resolve()
-		.then(() => {
-			opts.ReactDOM.unmountComponentAtNode(getRootDomEl(opts));
-		})
+  return Promise
+    .resolve()
+    .then(() => {
+      opts.ReactDOM.unmountComponentAtNode(getRootDomEl(opts));
+    })
 }
 
 function getRootDomEl(opts) {
-	const el = opts.domElementGetter();
-	if (!el) {
-		throw new Error(`single-spa-react: domElementGetter function did not return a valid dom element`);
-	}
+  const el = opts.domElementGetter();
+  if (!el) {
+    throw new Error(`single-spa-react: domElementGetter function did not return a valid dom element`);
+  }
 
-	return el;
+  return el;
 }
