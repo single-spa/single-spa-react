4bb350d4bf127d6cf62eac676301d4e21e538d84
Joel Denning <joeldenning@gmail.com>
2018-03-14 16:49:48 -0600

    Pass in the dom element getter as a custom prop instead of an opt (#29)
    * Add ability to pass in domElementGetter as a customProp instead of an
    opt.
    
    * Documentation update

--- src/single-spa-react.js
+++ src/single-spa-react.js
@@ -5,6 +5,9 @@ const defaultOpts = {
   rootComponent: null,
   domElementGetter: null,
   suppressComponentDidCatchWarning: false,
+
+  // optional opts
+  domElementGetter: null, // only can be omitted if provided as a custom prop
 }
 
 export default function singleSpaReact(userOpts) {
@@ -29,10 +32,6 @@ export default function singleSpaReact(userOpts) {
     throw new Error(`single-spa-react must be passed opts.rootComponent`);
   }
 
-  if (!opts.domElementGetter) {
-    throw new Error(`single-spa-react must be passed opts.domElementGetter function`);
-  }
-
   return {
     bootstrap: bootstrap.bind(null, opts),
     mount: mount.bind(null, opts),
@@ -46,26 +45,38 @@ function bootstrap(opts) {
 
 function mount(opts, props) {
   return new Promise((resolve, reject) => {
+    const domElementGetter = chooseDomElementGetter(opts, props)
+
+    if (!domElementGetter) {
+      throw new Error(`Cannot mount react application '${props.appName || props.name}' without a domElementGetter provided in either opts or props`)
+    }
+
     const whenFinished = function() {
       resolve(this);
     };
-    const renderedComponent = opts.ReactDOM.render(opts.React.createElement(opts.rootComponent, props), getRootDomEl(opts), whenFinished);
+    const renderedComponent = opts.ReactDOM.render(opts.React.createElement(opts.rootComponent, props), getRootDomEl(domElementGetter), whenFinished);
     if (!renderedComponent.componentDidCatch && !opts.suppressComponentDidCatchWarning && atLeastReact16(opts.React)) {
       console.warn(`single-spa-react: ${props.name || props.appName || props.childAppName}'s rootComponent should implement componentDidCatch to avoid accidentally unmounting the entire single-spa application.`);
     }
   })
 }
 
-function unmount(opts) {
+function unmount(opts, props) {
   return Promise
     .resolve()
     .then(() => {
-      opts.ReactDOM.unmountComponentAtNode(getRootDomEl(opts));
+      const domElementGetter = chooseDomElementGetter(opts, props)
+
+      if (!domElementGetter) {
+        throw new Error(`Cannot unmount react application '${props.appName || props.name}' without a domElementGetter provided in either opts or props`)
+      }
+
+      opts.ReactDOM.unmountComponentAtNode(getRootDomEl(domElementGetter));
     })
 }
 
-function getRootDomEl(opts) {
-  const el = opts.domElementGetter();
+function getRootDomEl(domElementGetter) {
+  const el = domElementGetter();
   if (!el) {
     throw new Error(`single-spa-react: domElementGetter function did not return a valid dom element`);
   }
@@ -84,4 +95,8 @@ function atLeastReact16(React) {
   } else {
     return false;
   }
+}
+
+function chooseDomElementGetter(opts, props) {
+  return props && props.customProps && props.customProps.domElementGetter ? props.customProps.domElementGetter : opts.domElementGetter
 }
