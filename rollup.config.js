d52f67e9c9d97ef69e1f8031991af1d88ecf6c28
madeofsun <rettou74@gmail.com>
2020-06-19 14:26:01 +0300

    ESM and SystemJS versions

--- rollup.config.js
+++ rollup.config.js
@@ -3,31 +3,87 @@ import { terser } from "rollup-plugin-terser";
 
 export default [
   {
-    input: "src/single-spa-react.js",
-    output: {
-      file: "lib/single-spa-react.js",
-      format: "umd",
-      name: "singleSpaReact",
-      sourcemap: true,
-    },
+    input: `src/single-spa-react.js`,
+    output: [
+      {
+        file: `lib/umd/single-spa-react.js`,
+        format: "umd",
+        name: "singleSpaReact",
+        sourcemap: true,
+      },
+      {
+        file: `lib/system/single-spa-react.js`,
+        format: "system",
+        sourcemap: true,
+      },
+      {
+        file: `lib/esm/single-spa-react.js`,
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
     plugins: [babel(), terser()],
   },
   {
-    input: "src/parcel.js",
+    input: "./src/single-spa-react.js",
     output: {
-      file: "parcel/index.js",
-      format: "umd",
-      name: "Parcel",
+      file: `./lib/es2015/single-spa-react.js`,
+      format: "esm",
       sourcemap: true,
-      globals: {
-        react: "React",
-        "single-spa-react": "singleSpaReact",
+    },
+    plugins: [
+      babel({
+        envName: "esm",
+      }),
+      terser({
+        ecma: 6,
+        module: true,
+      }),
+    ],
+  },
+  {
+    input: `src/parcel.js`,
+    output: [
+      {
+        file: `lib/umd/parcel.js`,
+        format: "umd",
+        name: "Parcel",
+        sourcemap: true,
+        globals: {
+          react: "React",
+          "single-spa-react": "singleSpaReact",
+        },
       },
-    },
+      {
+        file: `lib/system/parcel.js`,
+        format: "system",
+        sourcemap: true,
+      },
+      {
+        file: `lib/esm/parcel.js`,
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
     plugins: [babel(), terser()],
-    external: {
-      "single-spa-react": "single-spa-react",
-      React: "react",
+    external: ["single-spa-react", "react"],
+  },
+  {
+    input: "./src/parcel.js",
+    output: {
+      file: `./lib/es2015/parcel.js`,
+      format: "esm",
+      sourcemap: true,
     },
+    plugins: [
+      babel({
+        envName: "esm",
+      }),
+      terser({
+        ecma: 6,
+        module: true,
+      }),
+    ],
+    external: ["single-spa-react", "react"],
   },
 ];
