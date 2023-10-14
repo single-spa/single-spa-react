ac710c145651a93458b992eee52c21ee40174036
Milan Kovacic <MilanKovacic@users.noreply.github.com>
2023-10-14 06:41:32 +0200

    fix: issue with parcel exports/types

--- scripts/build-types.js
+++ scripts/build-types.js
@@ -1,17 +1,9 @@
 import fs from "fs";
-import rimraf from "rimraf";
 
 function copyFile(sourcePath, destinationPath) {
   fs.copyFileSync(sourcePath, destinationPath);
 }
 
-rimraf.sync("parcel");
-
-if (!fs.existsSync("parcel")) {
-  fs.mkdirSync("parcel");
-}
-
-copyFile("types/parcel/index.d.ts", "parcel/index.d.ts");
 copyFile("types/single-spa-react.d.ts", "types/single-spa-react.d.cts");
 
 // eslint-disable-next-line no-console
