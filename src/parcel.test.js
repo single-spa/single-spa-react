986149d04d4eac0a9b3d156935f621fc8f858c2f
Nick Bosland <nick.bosland@simacan.com>
2024-09-14 18:53:42 +0200

    Fixed an issue with mounting a parcel when StrictMode is enabled (#206)
    * Fixed an issue with mounting a parcel when StrictMode is enabled
    
    * Added changeset
    
    * Run parcel test with and without strict mode enabled
    
    * Properly setup the configuration of the react strict mode in th tests

--- src/parcel.test.js
+++ src/parcel.test.js
@@ -2,200 +2,213 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import * as ReactDOMClient from "react-dom/client"; // React >= 18
 import Parcel from "./parcel.js";
-import { render, waitFor } from "@testing-library/react";
+import { configure, render, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import singleSpaReact, { SingleSpaContext } from "./single-spa-react";
 import { jest } from "@jest/globals";
 
-describe(`<Parcel />`, () => {
-  let config,
-    mountParcel = jest.fn(),
-    parcel,
-    props;
+const cases = [{ strictMode: false }, { strictMode: true }];
 
-  beforeEach(() => {
-    config = {
-      bootstrap: jest.fn(),
-      mount: jest.fn(),
-      unmount: jest.fn(),
-    };
+describe.each(cases)(
+  `<Parcel />, strict mode $strictMode`,
+  ({ strictMode }) => {
+    let config,
+      mountParcel = jest.fn(),
+      parcel,
+      props;
 
-    parcel = {
-      loadPromise: jest.fn(),
-      bootstrapPromise: jest.fn(),
-      mountPromise: Promise.resolve(),
-      unmountPromise: jest.fn(),
-      getStatus: jest.fn(),
-      unmount: jest.fn(),
-      update: jest.fn(),
-    };
+    beforeEach(() => {
+      configure({ reactStrictMode: strictMode });
 
-    mountParcel.mockReset();
-    mountParcel.mockReturnValue(parcel);
+      config = {
+        bootstrap: jest.fn(),
+        mount: jest.fn(),
+        unmount: jest.fn(),
+      };
 
-    props = { mountParcel, config };
-    jest.spyOn(console, "error").mockReturnValue(undefined);
-  });
+      parcel = {
+        loadPromise: jest.fn(),
+        bootstrapPromise: jest.fn(),
+        mountPromise: Promise.resolve(),
+        unmountPromise: jest.fn(),
+        getStatus: jest.fn(),
+        unmount: jest.fn(),
+        update: jest.fn(),
+      };
 
-  it(`throws an error if you try to render the component without a config`, () => {
-    expect(() => {
-      render(<Parcel />);
-    }).toThrow();
-  });
+      mountParcel.mockReset();
+      mountParcel.mockReturnValue(parcel);
 
-  it(`renders a div by default`, () => {
-    expect(document.querySelector("div")).not.toBeInTheDocument();
-    const wrapper = render(<Parcel {...props} />);
-    expect(document.querySelector("div")).toBeInTheDocument();
-  });
+      props = { mountParcel, config };
+      jest.spyOn(console, "error").mockReturnValue(undefined);
+    });
 
-  it(`renders a div wrap with style`, () => {
-    const wrapper = render(
-      <Parcel {...props} wrapStyle={{ height: "100px" }} />
-    );
-    expect(document.querySelector(`div[style]`).style.height).toEqual("100px");
-  });
+    afterAll(() => {
+      configure({ strictMode: false });
+    });
 
-  it(`renders a div wrap with className`, () => {
-    const wrapper = render(<Parcel {...props} wrapClassName="wrapper" />);
-    expect(document.querySelector("div.wrapper")).toBeInTheDocument();
-  });
+    it(`throws an error if you try to render the component without a config`, () => {
+      expect(() => {
+        render(<Parcel />);
+      }).toThrow();
+    });
 
-  it(`calls the mountParcel prop when it mounts`, async () => {
-    const wrapper = render(<Parcel {...props} />);
-    await waitFor(() => expect(mountParcel).toHaveBeenCalled());
-  });
+    it(`renders a div by default`, () => {
+      expect(document.querySelector("div")).not.toBeInTheDocument();
+      const wrapper = render(<Parcel {...props} />);
+      expect(document.querySelector("div")).toBeInTheDocument();
+    });
 
-  it(`renders inside the append to`, async () => {
-    const appendTo = document.body.appendChild(
-      document.createElement("section")
-    );
-    expect(document.querySelector("section div")).not.toBeInTheDocument();
-    const wrapper = render(<Parcel {...props} appendTo={appendTo} />);
-    await waitFor(() =>
-      expect(document.querySelector("section div")).toBeInTheDocument()
-    );
-  });
+    it(`renders a div wrap with style`, () => {
+      const wrapper = render(
+        <Parcel {...props} wrapStyle={{ height: "100px" }} />
+      );
+      expect(document.querySelector(`div[style]`).style.height).toEqual(
+        "100px"
+      );
+    });
 
-  it(`calls parcelDidMount prop when the parcel finishes mounting`, async () => {
-    const parcelDidMount = jest.fn();
-    const wrapper = render(
-      <Parcel {...props} parcelDidMount={parcelDidMount} />
-    );
+    it(`renders a div wrap with className`, () => {
+      const wrapper = render(<Parcel {...props} wrapClassName="wrapper" />);
+      expect(document.querySelector("div.wrapper")).toBeInTheDocument();
+    });
 
-    expect(parcelDidMount).not.toHaveBeenCalled();
+    it(`calls the mountParcel prop when it mounts`, async () => {
+      const wrapper = render(<Parcel {...props} />);
+      await waitFor(() => expect(mountParcel).toHaveBeenCalled());
+    });
 
-    await waitFor(() => expect(parcelDidMount).toHaveBeenCalled());
-  });
+    it(`renders inside the append to`, async () => {
+      const appendTo = document.body.appendChild(
+        document.createElement("section")
+      );
+      expect(document.querySelector("section div")).not.toBeInTheDocument();
+      const wrapper = render(<Parcel {...props} appendTo={appendTo} />);
+      await waitFor(() =>
+        expect(document.querySelector("section div")).toBeInTheDocument()
+      );
+    });
 
-  it(`doesn't update the parcel a second or third time until previous parcel updates complete`, (done) => {
-    const wrapper = render(<Parcel {...props} />);
+    it(`calls parcelDidMount prop when the parcel finishes mounting`, async () => {
+      const parcelDidMount = jest.fn();
+      const wrapper = render(
+        <Parcel {...props} parcelDidMount={parcelDidMount} />
+      );
 
-    let numParcelUpdateCalls = 0;
-    let firstParcelUpdateFinished = false;
-    let secondParcelUpdateFinished = false;
+      expect(parcelDidMount).not.toHaveBeenCalled();
 
-    parcel.update.mockImplementation(() => {
-      switch (++numParcelUpdateCalls) {
-        case 1:
-          return firstParcelUpdate();
-        case 2:
-          return secondParcelUpdate();
-        case 3:
-          return thirdParcelUpdate();
-        default:
-          fail("Parcel update should only be called thrice");
-          break;
+      await waitFor(() => expect(parcelDidMount).toHaveBeenCalled());
+    });
+
+    it(`doesn't update the parcel a second or third time until previous parcel updates complete`, (done) => {
+      const wrapper = render(<Parcel {...props} />);
+
+      let numParcelUpdateCalls = 0;
+      let firstParcelUpdateFinished = false;
+      let secondParcelUpdateFinished = false;
+
+      parcel.update.mockImplementation(() => {
+        switch (++numParcelUpdateCalls) {
+          case 1:
+            return firstParcelUpdate();
+          case 2:
+            return secondParcelUpdate();
+          case 3:
+            return thirdParcelUpdate();
+          default:
+            fail("Parcel update should only be called thrice");
+            break;
+        }
+      });
+
+      function firstParcelUpdate() {
+        return new Promise((resolve) => {
+          /* Don't resolve this promise for a while to make sure that the second update
+           * Doesn't start until the first finishes
+           */
+          setTimeout(() => {
+            firstParcelUpdateFinished = true;
+            resolve();
+          }, 100);
+        });
       }
-    });
 
-    function firstParcelUpdate() {
-      return new Promise((resolve) => {
-        /* Don't resolve this promise for a while to make sure that the second update
-         * Doesn't start until the first finishes
-         */
-        setTimeout(() => {
-          firstParcelUpdateFinished = true;
-          resolve();
-        }, 100);
-      });
-    }
+      function secondParcelUpdate() {
+        return new Promise((resolve) => {
+          setTimeout(() => {
+            expect(firstParcelUpdateFinished).toBe(true);
+            secondParcelUpdateFinished = true;
+            resolve();
+          }, 100);
+        });
+      }
 
-    function secondParcelUpdate() {
-      return new Promise((resolve) => {
-        setTimeout(() => {
+      function thirdParcelUpdate() {
+        return Promise.resolve().then(() => {
           expect(firstParcelUpdateFinished).toBe(true);
-          secondParcelUpdateFinished = true;
-          resolve();
-        }, 100);
-      });
-    }
+          expect(secondParcelUpdateFinished).toBe(true);
+          done();
+        });
+      }
 
-    function thirdParcelUpdate() {
-      return Promise.resolve().then(() => {
-        expect(firstParcelUpdateFinished).toBe(true);
-        expect(secondParcelUpdateFinished).toBe(true);
-        done();
-      });
-    }
+      function triggerComponentDidUpdate() {
+        wrapper.rerender(<Parcel {...props} />);
+      }
 
-    function triggerComponentDidUpdate() {
-      wrapper.rerender(<Parcel {...props} />);
-    }
+      // not once
+      triggerComponentDidUpdate();
 
-    // not once
-    triggerComponentDidUpdate();
+      // not twice
+      triggerComponentDidUpdate();
 
-    // not twice
-    triggerComponentDidUpdate();
-
-    // but thrice!
-    triggerComponentDidUpdate();
-  });
-
-  it(`calls mountParcel with all the React props`, async () => {
-    const wrapper = render(<Parcel {...props} />);
-    // We need to wait for a microtask to finish before the Parcel component will have called mountParcel
-    await waitFor(() => expect(mountParcel).toHaveBeenCalled());
-    const parcelProps = mountParcel.mock.calls[0][1];
-    expect(parcelProps.domElement).toBeInstanceOf(HTMLDivElement);
-  });
-
-  it(`lets you not pass in a mountParcel prop if the SingleSpaContext is set with one (React <18)`, async () => {
-    // this creates the SingleSpaContext
-    singleSpaReact({
-      React,
-      ReactDOM,
-      rootComponent() {
-        return null;
-      },
+      // but thrice!
+      triggerComponentDidUpdate();
     });
 
-    render(
-      <SingleSpaContext.Provider value={{ mountParcel }}>
-        <Parcel config={config} />
-      </SingleSpaContext.Provider>
-    );
-
-    await waitFor(() => expect(mountParcel).toHaveBeenCalled());
-  });
-
-  it(`lets you not pass in a mountParcel prop if the SingleSpaContext is set with one (React >=18)`, async () => {
-    // this creates the SingleSpaContext
-    singleSpaReact({
-      React,
-      ReactDOMClient,
-      rootComponent() {
-        return null;
-      },
+    it(`calls mountParcel with all the React props`, async () => {
+      const wrapper = render(<Parcel {...props} />);
+      // We need to wait for a microtask to finish before the Parcel component will have called mountParcel
+      await waitFor(() => expect(mountParcel).toHaveBeenCalled());
+      const parcelProps = mountParcel.mock.calls[0][1];
+      expect(parcelProps.domElement).toBeInstanceOf(HTMLDivElement);
     });
 
-    const wrapper = render(
-      <SingleSpaContext.Provider value={{ mountParcel }}>
-        <Parcel config={config} />
-      </SingleSpaContext.Provider>
-    );
+    it(`lets you not pass in a mountParcel prop if the SingleSpaContext is set with one (React <18)`, async () => {
+      // this creates the SingleSpaContext
+      singleSpaReact({
+        React,
+        ReactDOM,
+        rootComponent() {
+          return null;
+        },
+      });
 
-    await waitFor(() => expect(mountParcel).toHaveBeenCalled());
-  });
-});
+      render(
+        <SingleSpaContext.Provider value={{ mountParcel }}>
+          <Parcel config={config} />
+        </SingleSpaContext.Provider>
+      );
+
+      await waitFor(() => expect(mountParcel).toHaveBeenCalled());
+    });
+
+    it(`lets you not pass in a mountParcel prop if the SingleSpaContext is set with one (React >=18)`, async () => {
+      // this creates the SingleSpaContext
+      singleSpaReact({
+        React,
+        ReactDOMClient,
+        rootComponent() {
+          return null;
+        },
+      });
+
+      const wrapper = render(
+        <SingleSpaContext.Provider value={{ mountParcel }}>
+          <Parcel config={config} />
+        </SingleSpaContext.Provider>
+      );
+
+      await waitFor(() => expect(mountParcel).toHaveBeenCalled());
+    });
+  }
+);
